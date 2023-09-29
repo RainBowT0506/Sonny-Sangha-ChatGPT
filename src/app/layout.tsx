5e341b9bf0ec708e1f97086f3054e887f90fe645
RainBowT <rainbowt0506@gmail.com>
2023-09-30 03:07:59 +0800

    [feat] Building the Login Screen & Functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default async function RootLayout({
 }) {
   const session = await getServerSession(authOptions);
 
+  console.log(session);
   return (
     <html lang="en">
       <body className={inter.className}>
