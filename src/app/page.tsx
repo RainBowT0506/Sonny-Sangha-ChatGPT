9c4bcbb5d73a2ebb04cf22960f24bff5cb02575f
RainBowT <rainbowt0506@gmail.com>
2023-09-29 22:55:52 +0800

    [lib] Implementing Heroicons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { SunIcon } from '@heroicons/react/24/outline'
+
 
 function HomePage() {
   return (
@@ -9,6 +11,8 @@ function HomePage() {
         <div>
           <div className="flex flex-col items-center justify-center mb-5">
             {/* Sun Icon */}
+            <SunIcon className="h-8 w-8" />
+
             <h2>Examples</h2>
           </div>
 
