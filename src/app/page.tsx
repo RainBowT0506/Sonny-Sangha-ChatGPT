7a007d417648784c82abcb94ddbbd22d1a8acd3d
RainBowT <rainbowt0506@gmail.com>
2023-09-29 23:08:00 +0800

    [feat] Building the Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,83 @@
-import React from 'react'
-import { SunIcon } from '@heroicons/react/24/outline'
-
+import {
+  SunIcon,
+  BoltIcon,
+  ExclamationTriangleIcon,
+} from "@heroicons/react/24/outline";
 
 function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
       <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
 
-      <div>
-        <div>
-          <div className="flex flex-col items-center justify-center mb-5">
-            {/* Sun Icon */}
-            <SunIcon className="h-8 w-8" />
+      <div className="flex space-x-2 text-center">
+        {renderExamples()}
 
-            <h2>Examples</h2>
-          </div>
+        {renderCapabilities()}
 
-          <div className="space-y-7">
-            <p className="infoText">"Explain Something to me"</p>
-            <p className="infoText">"What is the difference between a dog and a cat?"</p>
-            <p className="infoText">"What is the color of the sun?"</p>
-          </div>
-        </div>
+        {renderLimitations()}
       </div>
     </div>
-  )
+  );
+
+  function renderExamples() {
+    return (
+      <div>
+        <div className="flex flex-col items-center justify-center mb-5">
+          <SunIcon className="h-8 w-8" />
+
+          <h2>Examples</h2>
+        </div>
+
+        <div className="space-y-7">
+          <p className="infoText">"Explain Something to me"</p>
+          <p className="infoText">
+            "What is the difference between a dog and a cat?"
+          </p>
+          <p className="infoText">"What is the color of the sun?"</p>
+        </div>
+      </div>
+    );
+  }
+
+  function renderCapabilities() {
+    return (
+      <div>
+        <div className="flex flex-col items-center justify-center mb-5">
+          <BoltIcon className="h-8 w-8" />
+
+          <h2>Capabilities</h2>
+        </div>
+
+        <div className="space-y-7">
+          <p className="infoText">"Explain Something to me"</p>
+          <p className="infoText">
+            "What is the difference between a dog and a cat?"
+          </p>
+          <p className="infoText">"What is the color of the sun?"</p>
+        </div>
+      </div>
+    );
+  }
+
+  function renderLimitations() {
+    return (
+      <div>
+        <div className="flex flex-col items-center justify-center mb-5">
+          <ExclamationTriangleIcon className="h-8 w-8" />
+
+          <h2>Limitations</h2>
+        </div>
+
+        <div className="space-y-7">
+          <p className="infoText">"Explain Something to me"</p>
+          <p className="infoText">
+            "What is the difference between a dog and a cat?"
+          </p>
+          <p className="infoText">"What is the color of the sun?"</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default HomePage
+export default HomePage;
