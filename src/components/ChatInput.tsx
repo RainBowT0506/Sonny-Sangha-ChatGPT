69afc2ffb2a3048abb7312bc3326795c9df85eb5
RainBowT <rainbowt0506@gmail.com>
2023-10-05 00:14:24 +0800

    [feat] Building the Chat Input Component (1/2)

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -5,6 +5,8 @@ import { useSession } from "next-auth/react";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db } from "../../firebase";
 import toast from "react-hot-toast/headless";
+import ModelSelection from "./ModelSelection";
+import useSWR from "swr";
 
 type Props = {
   chatId: string;
@@ -14,8 +16,9 @@ function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState("");
   const { data: session } = useSession();
 
-  // useSWR to get model
-  const model = "text-davinci-003";
+  const { data: model } = useSWR("model", {
+    fallbackData: "text-davinci-003",
+  });
 
   const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -89,6 +92,10 @@ function ChatInput({ chatId }: Props) {
           <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
         </button>
       </form>
+
+      <div className="md:hidden">
+        <ModelSelection />
+      </div>
     </div>
   );
 }
