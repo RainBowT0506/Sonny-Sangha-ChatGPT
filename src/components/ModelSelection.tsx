2b5843b10d58fd6916ad129dc24903f4d37f2086
RainBowT <rainbowt0506@gmail.com>
2023-10-04 23:49:29 +0800

    [feat] Explaining & Implementing useSWR

--- src/components/ModelSelection.tsx
+++ src/components/ModelSelection.tsx
@@ -1,10 +1,12 @@
+"use client";
+
+import useSWR from "swr";
+
+const fetchModels = () => fetch("/api/getEngines").then((res) => res.json());
 
 function ModelSelection() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const { data: models, isLoading } = useSWR("models", fetchModels);
+  return <div>ModelSelection</div>;
 }
 
-export default ModelSelection
+export default ModelSelection;
