e6a3bc089849ebcdefd7f36a871cc25c31dfb6ca
RainBowT <rainbowt0506@gmail.com>
2023-10-05 00:09:42 +0800

    [feat] Building the Side Bar Component (5/5)

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -29,9 +29,16 @@ function SideBar() {
             <ModelSelection />
           </div>
 
-          {chats?.docs.map((chat) => (
-            <ChatRow key={chat.id} id={chat.id} />
-          ))}
+          <div className="flex flex-col space-y-2 my-2">
+            {loading && (
+              <div className="animate-pulse text-center text-white">
+                <p>Loading Chats...</p>
+              </div>
+            )}
+            {chats?.docs.map((chat) => (
+              <ChatRow key={chat.id} id={chat.id} />
+            ))}
+          </div>
         </div>
       </div>
 
